728cfb502aea27989c0711a4d1b841db8b61e42d
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-19 22:47:27 -0300

    Imported validation dependencies on route.js

--- routes/route.js
+++ routes/route.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
+const validateResult = require('../helpers/validate-routes');
 
 // write your routes here
 
